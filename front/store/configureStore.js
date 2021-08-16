ad4ccba4ffcf44aa71065b200f463e56ca042391
Kevin Kim <jhkevin21@gmail.com>
2021-08-15 19:28:28 -0600

    Added redux middleware

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,10 +1,18 @@
 import { createWrapper } from "next-redux-wrapper";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import reducer from "../reducers";
 
 const configureStore = () => {
-  const store = createStore(reducer);
+  //enhancer는 middleware 설치 위해 필요 함.
+  //아래는 reduxDevTools는 무조건 development 모드에서만 사용하기 (해킹의 위험이 있음). 따라서 production모드일때는 middleware를 사용안하게 설정 해 둠.
+  const middlewares = [];
+  const enhancer =
+    process.env.NODE_ENV === "production"
+      ? compose(applyMiddleware(...middlewares))
+      : composeWithDevTools(applyMiddleware(...middlewares));
+  const store = createStore(reducer, enhancer);
   return store;
 };
 const wrapper = createWrapper(configureStore, {
