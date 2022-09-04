9e26217d6cc154064eaa5d15e9e498146528de35
Jaehan-Kevin-Kim <jhkevin21@gmail.com>
2022-09-04 00:38:07 -0600

    backend settings change for production mode

--- back/app.js
+++ back/app.js
@@ -11,6 +11,8 @@ const userRouter = require("./routes/userRoute");
 const hashtagRouter = require("./routes/hashtagRoute");
 const db = require("./models");
 const passport = require("passport");
+const helmet = require("helmet");
+const hpp = require("hpp");
 const passportConfig = require("./passport");
 const app = express();
 
@@ -26,11 +28,18 @@ db.sequelize
 
 passportConfig();
 
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("combined")); // combined는 더 정확한 정보를 줌 (접속자의 ip등도 포함): 만약 특정 ip 접속자가 디도스와 같은 공격을 하는 경우 차단 가능 함. 따라서 배포용은 combined로 실행하기
+  app.use(hpp()); //hpp와 아래 helmet은 보안에 도움되는 package이므로 node에서 배포하는 경우는 무조건 이 두개는 필수라고 생각하고 넣어주기
+  app.use(helmet()); //helmet과 위 hpp는 보안에 도움되는 package이므로 node에서 배포하는 경우는 무조건 이 두개는 필수라고 생각하고 넣어주기
+} else {
+  app.use(morgan("dev"));
+}
 
 app.use(
   cors({
-    origin: true,
+    // origin: true,
+    origin: ["http://localhost:3050", "sharethought.com"], //원래는 origin: true로 사용했지만, localhost:3050으로 요청을 받았었다는 가정하에, 저 값을 놔두고, 추가로 추후에는 sharethought.com에서 요청을 받을 것이기 때문에 해당 값도 추가해 두기 (http or https 여부는 추후 결정될 것이므로 우선 붙이지 말기)
     credentials: true,
   }),
 );
@@ -44,6 +53,7 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.use(
   session({
+    // 만약 추후 https 적용한다면 그때 변경 하기
     saveUninitialized: false,
     resave: false,
     secret: process.env.COOKIE_SECRET,
