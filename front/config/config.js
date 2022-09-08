e211b68f6cfb744d6488acf0064f1854bb0060f1
Jaehan-Kevin-Kim <jhkevin21@gmail.com>
2022-09-07 22:45:28 -0600

    Fixed config.js typo, and added backend debug mode settings

--- front/config/config.js
+++ front/config/config.js
@@ -3,4 +3,4 @@ const prod = process.env.NODE_ENV === "production";
 
 export const backEndUrl = prod
   ? "http://api.sharethoughts.online" // For Production
-  : "http://localhost3065"; /// For Development
+  : "http://localhost:3065"; /// For Development
